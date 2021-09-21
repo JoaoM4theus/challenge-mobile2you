82022d2f3cdf50630a544cf3d8a10807a5568d6d
JoaoM4theus <j.matheusr@hotmail.com>
2021-09-21 19:33:05 -0300

    inital commit

---
+++ challenge-mobile2you/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge-mobile2you
+//
+//  Created by Softbuilder Hibrido on 21/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ challenge-mobile2youTests/challenge_mobile2youTests.swift
@@ -0,0 +1,33 @@
+//
+//  challenge_mobile2youTests.swift
+//  challenge-mobile2youTests
+//
+//  Created by Softbuilder Hibrido on 21/09/21.
+//
+
+import XCTest
+@testable import challenge_mobile2you
+
+class challenge_mobile2youTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
